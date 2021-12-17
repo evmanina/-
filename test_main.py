6d9b3df6e27a7d82eeb15af3cb386973a899f0f5
evmanina <90410981+evmanina@users.noreply.github.com>
2021-12-17 11:23:45 +0300

    Add files via upload
    https://drive.google.com/drive/folders/14DzZesJQyEislPOup0HqLeQcoF31qi0C?usp=sharing

---
+++ test_main.py
@@ -0,0 +1,10 @@
+import unittest
+from unittest.mock import patch
+
+
+class test_main(unittest.TestCase):
+    def test_1(running):
+        running.assertEqual(True,True)
+
+if __name__ == '__main__':
+    unittest.main()
